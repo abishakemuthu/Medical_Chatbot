b3a03bd0120017d49adf1cc2bff3f97ff199ddb3
abishake <abishakemuthu@gmail.com>
2024-11-15 19:40:31 +0530

    Project added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot Project',
+    version= '0.0.0',
+    author= 'Abishake',
+    author_email= 'abishakemuthu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
